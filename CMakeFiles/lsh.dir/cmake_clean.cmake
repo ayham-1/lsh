0fbb6dd5fb5e2d7e17df96c58d74181ea9513a08
Ayham Mamoun <ayhamaboualfadl@gmail.com>
2019-02-01 07:44:59 +0300

    Initial 2

---
+++ CMakeFiles/lsh.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lsh.dir/src/main.c.o"
+  "bin/lsh.pdb"
+  "bin/lsh"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lsh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
